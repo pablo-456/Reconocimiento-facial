5a35098bca5e1e4e7984e996f7ece84a8f249dd4
Juan Montoya <montoyajpmk@gmail.com>
2025-10-03 11:10:11 -0500

    añadir carpetas faltantes

---
+++ facial_recognition/Programa/versiones/alpha/1.py
@@ -0,0 +1,40 @@
+
+print("Codigo de Suma.")
+n=int(input("Ingrese valor: "))
+m=int(input("Ingrese valor: "))
+
+r=n+m
+
+def R(r):
+    r+=1
+    return r
+
+print()
+print("primer numero: ", n)
+print("segundo numero: ", m)
+print("Resultado de la suma: ", R(r))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
