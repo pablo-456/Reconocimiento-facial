070f29a786f807a62f223625271aeba93d92fbc0
Juan Montoya <montoyajpmk@gmail.com>
2025-10-09 18:27:05 -0500

    Eliminando paquetes viejos

--- facial_recognition/cosas_importantes/cosas_importantes.py
+++ facial_recognition/cosas_importantes/cosas_importantes.py
@@ -2,3 +2,6 @@
 #Creas entorno virtual: python -m venv tf-env
 #Iniciar entorno virtual: .\tf-env\Scripts\activate
 #Nota: se deve instalar las herramientas a usar en el entorno
+
+#Instalar con pip install: pip install opencv-python mediapipe imutils pymongo numpy
+#: pip install opencv-contrib-python
